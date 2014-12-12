4e2d9d32916d96be002dbe635e762ad4805fb7db
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-12-12 20:47:18 -0300

    Manage clipboard contents

--- copy_from_find_in_files_command.py
+++ copy_from_find_in_files_command.py
@@ -2,12 +2,18 @@ import sublime, sublime_plugin, re
 
 class CopyFromFindInFilesCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if not self.in_find_results_view():
-            print("nay")
-            return self.view.run_command("copy")
+        self.view.run_command("copy")
 
-        print("yay")
+        if not self.in_find_results_view():
+            return
+
         self.settings = sublime.load_settings("CopyFromFindInFiles.sublime-settings")
+        clipboard_contents = sublime.get_clipboard()
+
+        if clipboard_contents:
+            re.sub(r'insert_regex_here', '', clipboard_contents)
+            sublime.set_clipboard(clipboard_contents)
 
     def in_find_results_view(self):
         return self.view.settings().get('syntax') == 'Packages/Default/Find Results.hidden-tmLanguage'
+
