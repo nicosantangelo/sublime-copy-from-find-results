30fcecc1ce6aa19e0f2497d0ba1363a6d9ce5eb8
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-12-16 00:49:02 -0300

    Keep intermediate dots option

--- copy_from_find_in_files_command.py
+++ copy_from_find_in_files_command.py
@@ -1,17 +1,22 @@
 import sublime, sublime_plugin, re
 
 class CopyFromFindInFilesCommand(sublime_plugin.TextCommand):
+    default_regex = r'^\s*\d+\:?'
+    without_dots_regex = r'^\s*(\d+\:?|.+)'
+
     def run(self, edit):
-        self.view.run_command("copy")
+        self.view.run_command('copy')
 
         if not self.in_find_results_view():
             return
 
-        self.settings = sublime.load_settings("CopyFromFindInFiles.sublime-settings")
+        self.settings = sublime.load_settings('CopyFromFindInFiles.sublime-settings')
+
         clipboard_contents = sublime.get_clipboard()
 
         if clipboard_contents:
-            new_clipboard = re.sub(r'^\s*\d+\:?', '', clipboard_contents, flags=re.MULTILINE)
+            regex = CopyFromFindInFilesCommand.default_regex if self.settings.get('keep_intermediate_dots', True) else CopyFromFindInFilesCommand.without_dots_regex
+            new_clipboard = re.sub(regex, '', clipboard_contents, flags=re.MULTILINE)
             sublime.set_clipboard(new_clipboard)
 
     def in_find_results_view(self):
