25dd3835bc096fc80b64c13dcd9dc16edfd70514
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-12-16 23:16:02 -0300

    Refactor

--- copy_from_find_in_files_command.py
+++ copy_from_find_in_files_command.py
@@ -1,24 +1,31 @@
 import sublime, sublime_plugin, re
 
 class CopyFromFindInFilesCommand(sublime_plugin.TextCommand):
-    default_regex = r'^\s*\d+\:?'
-    without_dots_regex = r'^\s*(\d+\:?|.+)'
-
     def run(self, edit):
         self.view.run_command('copy')
 
         if not self.in_find_results_view():
             return
 
-        self.settings = sublime.load_settings('CopyFromFindInFiles.sublime-settings')
-
         clipboard_contents = sublime.get_clipboard()
 
         if clipboard_contents:
-            regex = CopyFromFindInFilesCommand.default_regex if self.settings.get('keep_intermediate_dots', True) else CopyFromFindInFilesCommand.without_dots_regex
-            new_clipboard = re.sub(regex, '', clipboard_contents, flags=re.MULTILINE)
+            keep_intermediate_dots = sublime.load_settings('CopyFromFindInFiles.sublime-settings').get('keep_intermediate_dots', False)
+            new_clipboard = RegexStruct(keep_intermediate_dots).sub(clipboard_contents)
             sublime.set_clipboard(new_clipboard)
 
     def in_find_results_view(self):
         return self.view.settings().get('syntax') == 'Packages/Default/Find Results.hidden-tmLanguage'
 
+class RegexStruct():
+    default = r'^\s*\d+\:?'
+    without_dots = r'^\s*(\d+\:?|.+)'
+
+    def __init__(self, keep_dots=True):
+        self.keep_dots = keep_dots
+
+    def sub(self, text):
+        return re.sub(self.construct(), '', text, flags=re.MULTILINE)
+
+    def construct(self):
+        return RegexStruct.default if self.keep_dots else RegexStruct.without_dots
